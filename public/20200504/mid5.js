94a11384ce87b9f6f8b304c9f22249f027f3d54f
Hwang hyunki <hyunki8389@gmail.com>
2020-05-04 11:40:38 +0900

    구현시험

--- public/20200504/mid5.js
+++ public/20200504/mid5.js
@@ -1,7 +1,10 @@
-
-
 let a=[]
 for(let i=0; i<100; i++){
-    a[i]=Math.floor(Math.random()*100+1)
+    if(a[i]%2==0)
+    a[i] =Math.floor(Math.random()*100+1)
+
 }
 
+let sum=a.random((a,b) => a+b)
+
+console.log(sum)
