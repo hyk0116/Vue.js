803d10721c8f65e10f8e6e42c92746c778844236
Hwang hyunki <hyunki8389@gmail.com>
2020-05-04 10:02:00 +0900

    구현시험

--- public/20200504/mid4.js
+++ public/20200504/mid4.js
@@ -1,7 +1,16 @@
-function sum(a[]){
-    sum=0;
-    for(let i=0; i<10; i++){
+function sum(...a){
+    let sum=0;
+    for(let i=0; i<a.length; i++){
         sum+=a[i]
+        return sum
     }
 }
-function test_sum[]
+function text_sum(...a){
+    let b= []
+    for(let i=0; i<10; i++)
+    b[i]= Math.floor(Math.random()*5+1)
+   
+    for(let i=0; i<10; i++)
+    console.log(b[i])
+    return b;
+}
